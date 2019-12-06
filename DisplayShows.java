9052406061603292e924e26f099fc68a4998ef38
mfbernasol <46685981+mfbernasol@users.noreply.github.com>
2019-12-06 14:39:12 -0600

    created requestShowData function

--- DisplayShows.java
+++ DisplayShows.java
@@ -3,6 +3,43 @@ import java.util.ArrayList;
 import javax.swing.JTextArea;
 
 public class DisplayShows {
+    
+        public static ArrayList<TvShow> requestShowData(String show) {
+        ArrayList<TvShow> shows = new ArrayList<>();
+        String urlString = "http://api.tvmaze.com/singlesearch/shows?q= " + show;
+        JSONParser parser = new JSONParser();
+        try {
+
+            URL url = new URL(urlString);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            JSONObject jObj = (JSONObject) parser.parse(reader);
+
+            String showName = (String) jObj.get("name");
+
+            //  ArrayList<String> genres = (JSONArray)jObj.get("genres");
+
+            String language = (String) jObj.get("language");
+
+            String status = (String) jObj.get("status");
+
+            String summary = (String) jObj.get("summary");
+            summary = summary.replace(".", "." + "\n").replaceAll("\\<.*?>", "");// removes html tags
+            shows.add(new TvShow(showName, language, status, summary));
+
+
+            reader.close();
+            return shows;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+    }
+
+
+    
+    
     public static String printShowsToScreen(ArrayList<TvShow> tvShows){
         String text = "";
     	for(TvShow t: tvShows){
